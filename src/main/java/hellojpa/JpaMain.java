0db289ef5badae8209d1150c04f54f085611ce2f
Hangsuu <ohhangsu@naver.com>
2024-02-19 10:44:54 +0900

    영속성 컨텍스트

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -16,11 +16,22 @@ public class JpaMain {
         //code
         try {
             // 입력
+            // 비영속
             Member member = new Member();
-            member.setId(1L);
-            member.setName("HelloA");
-            em.persist(member);
+            member.setId(101L);
+            member.setName("HelloJPA");
+            // 영속(영속 상태가 돼도 바로 DB에 쿼리가 날아가지 않는다
+            System.out.println("===========BEFORE===========");
+//            em.persist(member);
+            System.out.println("===========AFTER===========");
 
+            Member findMember1 = em.find(Member.class, 101L);
+            Member findMember2 = em.find(Member.class, 101L);
+
+            System.out.println("findMember.id = " + findMember1.getId());
+            System.out.println("findMember.name = " + findMember2.getName());
+
+            System.out.println("result = " + (findMember1 == findMember2));
             // 수정
 //            Member findmember = em.find(Member.class, 1L);
 //            findmember.setName("HelloJPA");
@@ -33,7 +44,7 @@ public class JpaMain {
 //            for (Member member : result) {
 //                System.out.println("member.name = " + member.getName());
 //            }
-
+            // commit 상태에 db에 쿼리가 날아감
             tx.commit();
         } catch(Exception e) {
             tx.rollback();
