b6d246cd9214103e2962bee89fcea8698394dd4b
Administrator <admin@example.com>
2024-02-27 14:36:41 +0900

    Named 쿼리

--- jpql/src/main/java/hellojpa/jpql/Member.java
+++ jpql/src/main/java/hellojpa/jpql/Member.java
@@ -4,6 +4,10 @@ import jakarta.persistence.*;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @Entity
+@NamedQuery(
+        name = "Member.findByUsername",
+        query = "select m from Member m where m.username = :username"
+)
 public class Member {
     @Id @GeneratedValue
     private Long id;
