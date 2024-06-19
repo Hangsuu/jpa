82bf167bdb13680e8726e0df829dad11935dd5ff
hangsuu <ohhangsu@naver.com>
2024-06-19 21:11:12 +0900

    엔티티 클래스 개발2

--- jpashop2/src/main/java/jpabook/jpashop/domain/Address.java
+++ jpashop2/src/main/java/jpabook/jpashop/domain/Address.java
@@ -9,4 +9,15 @@ public class Address {
     private String cty ;
     private String street;
     private String zipcode;
+
+    // Setter를 사용하지 않을 경우 생성자로 해결
+    // 임베디드 타입 기본 생성자는 JPA 스펙 상 public 또는 protected로 설정해야됨
+    protected Address() {
+    }
+
+    public Address(String cty, String street, String zipcode) {
+        this.cty = cty;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
